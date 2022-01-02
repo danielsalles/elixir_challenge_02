02cfa5704d094e36156645636a7d641abfde188f
Daniel Bastos <danielsalles@gmail.com>
2022-01-02 14:15:22 -0300

    added test

--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -1,8 +1,17 @@
 defmodule ListFilterTest do
   use ExUnit.Case
-  doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  describe "call/1" do
+    test "return list" do
+      list = [1, 2, 3, 4, 5, "banana", 10, "10", 100, "100"]
+
+      assert ListFilter.call(list) === [2, 4, 10, 100]
+    end
+
+    test "passing an empty list" do
+      list = []
+
+      assert ListFilter.call(list) == []
+    end
   end
 end
